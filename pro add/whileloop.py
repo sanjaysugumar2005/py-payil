8da11f02b575bf1eb385bf92cba025a25d81045e
sanjay <sanjaysugumar2005@gmail.com>
2026-03-04 00:27:31 +0530

    todaywhile

---
+++ pro add/whileloop.py
@@ -0,0 +1,47 @@
+    #    QUTION
+    #         OUTPUT
+    #             1,2,3,4,5
+    #             2,4,6,8,10
+    #             1,3,5,7,9
+    #             5,10,15,20,25
+
+
+# x=0
+# while x<=4:
+#     x=x+1
+#     print(x)
+
+# x=0
+# while x<=9:
+#     x=x+2
+#     print(x)
+
+# x=1
+# while x<=8:
+#     x=x+2
+#     print(x)
+    #             1,2,3,4,5
+# x=1
+# while x<=5:
+#     print(x)
+#     x=x+1
+
+    #             2,4,6,8,10
+# x=2
+# while x<=10:
+#     print(x)
+#     x=x+2
+
+   #             1,3,5,7,9
+
+# x=1
+# while x<=9:
+#     print(x)
+#     x=x+2
+
+    #             5,10,15,20,25
+
+# x=5
+# while x<=25:
+#     print(x)
+#     x=x+5
