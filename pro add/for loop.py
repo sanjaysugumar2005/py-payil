005fc014679d64e59fdf33aef30ef0c709a1191a
sanjay <sanjaysugumar2005@gmail.com>
2026-03-07 00:06:53 +0530

    todaytest

---
+++ pro add/for loop.py
@@ -0,0 +1,82 @@
+# for x in  range (1,11,1):
+#     print(x)
+
+# for x in range (10,-1,-2):
+#     print(x)
+
+# for x in range(25,-1,-5):
+#     print(x)
+
+# for i in range (1,11,1):
+#     print(i)
+
+############################## FOR LOOP 8 QUETION #################################
+
+
+# 1. Print 1, 2, 3 … 10
+
+# def add():
+#     for x in range(1,11,1):
+#         print(x)
+# add()
+
+
+# 2. Print 2, 4 ,6 … 20
+
+# def mul():
+#     for x in range(2,21,2):
+#         print(x)
+# mul()
+
+
+# 3. Print  Table given a number 
+#  a. Ex: 2 -> 1x2, 2x2, 3x2 … 10x2
+
+# def table():
+#     for x in range(1,11):
+#         print(x,"x",2,"=",x*2)
+# table()
+
+# 4. Find all odd numbers until given number
+#  a. Ex: 10 -> 1, 3, 5, 7, 9
+# def odd():
+#     for x in range(1,11):
+#         if x%2!=0:
+#             print("odd",x)
+# odd()
+
+# 5.Find all even numbers until given number
+#   Ex: 10 -> 1, 3, 5, 7, 9
+# def even():
+#     for x in range(1,11):
+#         if x%2==0:
+#             print("even",x)
+# even()
+
+# 5. Find all numbers divisible by 3 until given number
+#  a. Ex: 10 -> 3,  6, 9
+
+# def divi():
+#     for x in range(1,10):
+#         if x%3==0:
+#             print(x)
+# divi()
+
+# 6. Find all numbers divisible by 3 and 5 until given number
+#  a. Ex: 50 -> 15, 30, 45
+
+# def divi():
+#     for x in range(1,50):
+#         if x%3==0 and x%5==0:
+#             print(x)
+# divi()
+
+# 7. Sum of natural numbers until given number
+#  a. Ex: input:  4  ->  1 + 2 + 3 + 4 ->  10
+
+# def nat():
+#     total=0
+#     for x in range(1,5):
+#         total=total+x
+#         print(total)
+# nat()
